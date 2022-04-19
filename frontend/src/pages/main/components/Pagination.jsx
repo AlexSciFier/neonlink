45f8b404b09a88e72d063a3a3310e4c5f3b10908
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-04-19 10:30:11 +0300

    changed pagination design

--- frontend/src/pages/main/components/Pagination.jsx
+++ frontend/src/pages/main/components/Pagination.jsx
@@ -1,6 +1,36 @@
 import React from "react";
-import { Navigate, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { createSearchParams } from "react-router-dom";
+
+function ChevronLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+    </svg>
+  );
+}
+
+function ChevronRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+    </svg>
+  );
+}
 
 function PaginationButton({ children, disabled, isCurrent, onClick }) {
   return (
@@ -46,13 +76,13 @@ export default function Pagination({ maxPage, currentPage, siblings }) {
   return (
     <div className="flex justify-center gap-3">
       <button
-        className={`p-2 w-10 h-10 rounded-full hover:bg-gray-200 ${
+        className={`p-2 w-10 h-10 rounded-full text-gray-600 hover:bg-gray-200 ${
           isFirst ? "pointer-events-none" : ""
         }`}
         disabled={isFirst}
         onClick={onPrevClick}
       >
-        {"<"}
+        <ChevronLeft />
       </button>
       {range(maxPage, currentPage, siblings).map((value, idx) => (
         <PaginationButton
@@ -65,13 +95,13 @@ export default function Pagination({ maxPage, currentPage, siblings }) {
         </PaginationButton>
       ))}
       <button
-        className={`p-2 w-10 h-10 rounded-full hover:bg-gray-200 ${
+        className={`p-2 w-10 h-10 rounded-full text-gray-600 hover:bg-gray-200 ${
           isLast ? "pointer-events-none" : ""
         }`}
         disabled={isLast}
         onClick={onNextClick}
       >
-        {">"}
+        <ChevronRight />
       </button>
     </div>
   );
