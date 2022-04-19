c458d3b9dcf0e3b525a3ee33a0fef7afb967e328
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-04-19 13:40:01 +0300

    add Heroicon library

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { LogoutIcon, PlusIcon } from "@heroicons/react/outline";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useIsloggedIn } from "../context/isLoggedIn";
@@ -14,40 +15,14 @@ export default function NavBar() {
           to={"/add"}
           className="bg-cyan-500 text-white px-4 h-full rounded flex items-center gap-1"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4v16m8-8H4"
-            />
-          </svg>
+          <PlusIcon className="w-6 h-6" />
           Add bookmark
         </Link>
         <Link className="leading-loose" to={"/settings"}>
           {profile.username}
         </Link>
         <Link to={"/logout"}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-            />
-          </svg>
+          <LogoutIcon className="w-6 h-6" />
         </Link>
       </div>
     </div>
--- frontend/src/pages/main/components/Pagination.jsx
+++ frontend/src/pages/main/components/Pagination.jsx
@@ -1,36 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { createSearchParams } from "react-router-dom";
-
-function ChevronLeft() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-    </svg>
-  );
-}
-
-function ChevronRight() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-    </svg>
-  );
-}
+import { ChevronRightIcon } from "@heroicons/react/outline";
+import { ChevronLeftIcon } from "@heroicons/react/outline";
 
 function PaginationButton({ children, disabled, isCurrent, onClick }) {
   return (
@@ -82,7 +54,7 @@ export default function Pagination({ maxPage, currentPage, siblings }) {
         disabled={isFirst}
         onClick={onPrevClick}
       >
-        <ChevronLeft />
+        <ChevronLeftIcon />
       </button>
       {range(maxPage, currentPage, siblings).map((value, idx) => (
         <PaginationButton
@@ -101,7 +73,7 @@ export default function Pagination({ maxPage, currentPage, siblings }) {
         disabled={isLast}
         onClick={onNextClick}
       >
-        <ChevronRight />
+        <ChevronRightIcon />
       </button>
     </div>
   );
