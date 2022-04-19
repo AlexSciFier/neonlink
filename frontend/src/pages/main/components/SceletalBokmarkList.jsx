e443bc7140a5b36116dcc73b12f09869d9358df1
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-04-19 15:54:58 +0300

    fixed sceletal loader

--- frontend/src/pages/main/components/SceletalBokmarkList.jsx
+++ frontend/src/pages/main/components/SceletalBokmarkList.jsx
@@ -1,12 +1,23 @@
+import { ChevronRightIcon } from "@heroicons/react/outline";
 import React from "react";
 
 export default function SceletalBokmarkList() {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col  divide-y">
       {[...Array(10).keys()].map((idx) => (
-        <div className="w-full animate-pulse space-y-2" key={idx}>
-          <div className="bg-gray-200 rounded-lg h-4"></div>
-          <div className="bg-gray-200 rounded-lg h-8"></div>
+        <div className="flex items-center animate-pulse">
+          <div className="flex flex-1 items-center gap-3">
+            <div>
+              <div className="w-8 h-8 rounded bg-gray-200"></div>
+            </div>
+            <div className="w-full py-2 space-y-2" key={idx}>
+              <div className="bg-gray-200 rounded-lg h-4"></div>
+              <div className="bg-gray-200 rounded-lg h-8"></div>
+            </div>
+          </div>
+          <div>
+            <ChevronRightIcon className="w-8 h-8 text-gray-200" />
+          </div>
         </div>
       ))}
     </div>
