6b93fa118f157a5faf5c0569f3e43add01bc555d
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-04-22 14:10:52 +0300

    some ui tweaks

--- frontend/src/pages/main/components/SceletalBokmarkList.jsx
+++ frontend/src/pages/main/components/SceletalBokmarkList.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 export default function SceletalBokmarkList() {
   return (
-    <div className="flex flex-col  divide-y">
+    <div className="flex flex-col divide-y">
       {[...Array(10).keys()].map((idx) => (
-        <div className="flex items-center animate-pulse">
+        <div className="flex items-center animate-pulse" key={idx}>
           <div className="flex flex-1 items-center gap-3">
             <div>
               <div className="w-8 h-8 rounded bg-gray-200"></div>
             </div>
-            <div className="w-full py-2 space-y-2" key={idx}>
+            <div className="w-full py-2 space-y-2">
               <div className="bg-gray-200 rounded-lg h-4"></div>
               <div className="bg-gray-200 rounded-lg h-8"></div>
             </div>
