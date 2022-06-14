94df83ed106f51ce83c7f0dd0c637725836c948d
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-06-14 17:28:48 +0300

    server listen address fix

--- frontend/src/helpers/fetch.js
+++ frontend/src/helpers/fetch.js
@@ -1,5 +1,6 @@
-const BASE_URL = "http://localhost:3333";
-//window.location.protocol + "//" + window.location.hostname + ":3333";
+//const BASE_URL = "http://localhost:3333";
+const BASE_URL =
+  window.location.protocol + "//" + window.location.hostname + ":3333";
 
 export async function postJSON(endpoint, json) {
   let url = new URL(endpoint, BASE_URL).toString();
