99acefcd7c168eacb4210ab15e3a11a955542f0f
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-06-24 17:56:40 +0300

    change tag suggestion list

--- frontend/src/pages/addBookmark/components/TagInput.jsx
+++ frontend/src/pages/addBookmark/components/TagInput.jsx
@@ -41,34 +41,49 @@ export default function TagInput({ tags, setTags }) {
   }
 
   return (
-    <div className="flex items-center rounded border focus-within:ring-cyan-600 focus-within:ring bg-transparent dark:text-white">
-      <div className="flex gap-2 px-2">
-        {tags.map((tag, idx) => (
-          <div
-            className="inline-flex whitespace-nowrap px-2 bg-cyan-700 text-white rounded"
-            key={idx}
-          >
-            {tag}
-            <button onClick={(e) => deleteTag(e, tag)} className="pl-2">
-              ×
-            </button>
-          </div>
-        ))}
+    <div className="relative">
+      <div className="flex items-center rounded border focus-within:ring-cyan-600 focus-within:ring bg-transparent dark:text-white">
+        <div className="flex gap-2 px-2">
+          {tags.map((tag, idx) => (
+            <div
+              className="inline-flex whitespace-nowrap px-2 bg-cyan-700 text-white rounded"
+              key={idx}
+            >
+              {tag}
+              <button onClick={(e) => deleteTag(e, tag)} className="pl-2">
+                ×
+              </button>
+            </div>
+          ))}
+        </div>
+        <input
+          className="w-full focus:outline-none py-2 bg-transparent dark:text-white"
+          type={"text"}
+          list={"tags"}
+          placeholder="Tags"
+          name={"tags"}
+          value={tagInput}
+          autoComplete="off"
+          onChange={(e) => setTagInput(e.target.value)}
+        ></input>
       </div>
-      <input
-        className="w-full focus:outline-none py-2 bg-transparent dark:text-white"
-        type={"text"}
-        list={"tags"}
-        placeholder="Tags"
-        name={"tags"}
-        value={tagInput}
-        onChange={(e) => setTagInput(e.target.value)}
-      ></input>
-      <datalist id="tags">
-        {suggestions.map((suggestion, idx) => (
-          <option key={idx} value={suggestion.name} />
-        ))}
-      </datalist>
+      {suggestions.length > 0 && (
+        <ul className="absolute bg-white text-black left-0 right-0 mt-1 py-1 rounded shadow dark:ring-1 dark:ring-white/10 dark:bg-gray-900 dark:text-white dark:shadow-cyan-500/20">
+          {suggestions.map((suggestion, idx) => (
+            <li
+              key={idx}
+              onClick={() => {
+                setTags([...tags, suggestion.name]);
+                setTagInput("");
+                setSuggestions([]);
+              }}
+              className="px-4 hover:bg-cyan-500 cursor-pointer"
+            >
+              {suggestion.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
