666e616a2e1f2ebb148f03aa8c1d088d45193176
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-06-27 13:29:21 +0300

    botttom margin in page

--- frontend/src/components/PageBody.jsx
+++ frontend/src/components/PageBody.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PageBody({ children }) {
   return (
-    <div className="flex gap-3 rounded-xl h-full md:p-4 mx-3 dark:bg-gray-900 bg-white dark:text-white flex-1">
+    <div className="flex gap-3 rounded-xl h-full md:p-4 mx-3 mb-2 dark:bg-gray-900 bg-white dark:text-white flex-1">
       {children}
     </div>
   );
