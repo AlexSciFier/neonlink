5505bf1c430729ad2e81e128fec5d1d47ea4e53b
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-06-27 13:28:50 +0300

    new font for navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./fonts/Raleway-Light.ttf";
+import "./fonts/Raleway-Medium.ttf";
+import "./fonts/Raleway-Regular.ttf";
 import App from "./App";
 import { IsLoggedInProvider } from "./context/isLoggedIn";
 import { ThemeProvider } from "./context/themeContext";
