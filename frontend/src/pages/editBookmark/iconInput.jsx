f71cfbfb0925a8a6a8a76e693658fea28e6d7ad4
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-08-17 15:37:47 +0300

    fix icon loading

--- frontend/src/pages/editBookmark/iconInput.jsx
+++ frontend/src/pages/editBookmark/iconInput.jsx
@@ -1,18 +1,35 @@
 import { DownloadIcon } from "@heroicons/react/outline";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router";
 import { postJSON } from "../../helpers/fetch";
 
 export default function IconInput({ url, icon, setIcon }) {
+  const [defaultIconUrl, setDefaultIconUrl] = useState();
+  const { id } = useParams();
   async function fetchIcon() {
     const res = await postJSON("/api/utils/urlinfo", { url });
     const json = await res.json();
     setIcon(json.icon);
   }
 
+  useEffect(() => {
+    if (icon === undefined || icon === "") {
+      setDefaultIconUrl(
+        `${
+          process.env.NODE_ENV === "production" ? "" : "http://localhost:3333"
+        }/api/bookmarks/${id}/icon`
+      );
+    } else {
+      setDefaultIconUrl();
+    }
+  }, [icon, id]);
+
   return (
     <div className="flex gap-1 items-center w-full bg-transparent rounded border">
       <div
-        style={{ backgroundImage: `url(${icon})` }}
+        style={{ backgroundImage: `url(${defaultIconUrl || icon})` }}
         className="w-6 h-6 bg-cover flex-none m-1"
       ></div>
       <DownloadIcon
