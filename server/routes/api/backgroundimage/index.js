492c5e5cb85555d883e03fad50ace3628c5f2db7
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-10-21 13:20:58 +0300

    move preHandler in sepparate file

--- server/routes/api/backgroundimage/index.js
+++ server/routes/api/backgroundimage/index.js
@@ -1,25 +1,7 @@
 const { default: fastify } = require("fastify");
 const bgImages = require("../../../db/bgImages");
 const usersDB = require("../../../db/users");
-
-/**
- *
- * @param {import("fastify").FastifyRequest} request
- * @param {import("fastify").FastifyReply} reply
- */
-async function requestForbidden(request, reply) {
-  try {
-    let SSID = request.cookies.SSID;
-    if (SSID) {
-      let user = await usersDB.getUserByUUID(SSID);
-      if (user === undefined) {
-        throw reply.unauthorized("You must login to use this method");
-      }
-    } else throw reply.unauthorized("You must login to use this method");
-  } catch {
-    throw reply.unauthorized("You must login to use this method");
-  }
-}
+const { requestForbidden } = require("../utils/preHandler");
 
 /**
  *
---
+++ server/routes/api/utils/preHandler.js
@@ -0,0 +1,27 @@
+const usersDB = require("../../../db/connect");
+const appSettings = require("../../../db/appSettings");
+/**
+ *
+ * @param {import("fastify").FastifyRequest} request
+ * @param {import("fastify").FastifyReply} reply
+ */
+async function requestForbidden(request, reply) {
+  try {
+    const noLogin = appSettings.getNologin();
+    console.log("PREHANDLER", noLogin);
+    if (noLogin) {
+      return;
+    }
+    let SSID = request.cookies.SSID;
+    if (SSID) {
+      let user = await usersDB.getUserByUUID(SSID);
+      if (user === undefined) {
+        throw reply.unauthorized("You must login to use this method");
+      }
+    } else throw reply.unauthorized("You must login to use this method");
+  } catch {
+    throw reply.unauthorized("You must login to use this method");
+  }
+}
+
+module.exports = { requestForbidden };
