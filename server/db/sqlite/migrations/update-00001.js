f3d0ae5de3358ac6e45d053a6b207a398ba34277
julichan <julichan@faelyn.net>
2023-07-10 00:42:53 +0200

    revert unwanted changes

--- server/db/sqlite/migrations/update-00001.js
+++ server/db/sqlite/migrations/update-00001.js
@@ -1,8 +1,8 @@
 import { columnExists } from "../common.js";
 
 export default async function (db) {
-  if (columnExists(db, "backgrounds", "uuid") === false) {
-    db.prepare("ALTER TABLE backgrounds ADD COLUMN uuid TEXT").run();
+  if (columnExists(db, "bgImages ", "uuid") === false) {
+    db.prepare("ALTER TABLE bgImages ADD COLUMN uuid TEXT").run();
   }
 
   const statment = `CREATE TABLE IF NOT EXISTS migrations (name TEXT PRIMARY KEY, version INTEGER)`;
