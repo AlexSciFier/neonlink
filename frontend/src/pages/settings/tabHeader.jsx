253bb379624852f53151e4d615f35bfc6e65b123
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2023-09-08 17:18:25 +0300

    fix: consistent settings tabs

--- frontend/src/pages/settings/tabHeader.jsx
+++ frontend/src/pages/settings/tabHeader.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 export default function TabHeader({ title, isActive, onClick }) {
   return (
-    <li
+    <a
       className={`px-4 py-2 hover:bg-cyan-600/10 hover:cursor-pointer ${
         isActive ? "border-b-2 border-cyan-500" : ""
       }`}
       onClick={onClick}
+      href={`#${title}`}
     >
       {title}
-    </li>
+    </a>
   );
 }
--- frontend/src/pages/settings/tabView.jsx
+++ frontend/src/pages/settings/tabView.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TabHeader from "./tabHeader";
 import AboutTab from "./tabs/aboutTab";
 import GroupTab from "./tabs/groupTab/groupTab";
 import MainTab from "./tabs/mainTab/mainTab";
 import InterfaceTab from "./tabs/interfaceTab/interfaceTab";
+import { useLocation } from "react-router";
 
 export default function TabView() {
+  const location = useLocation();
   const tabs = [
     {
       header: "Main",
@@ -26,6 +28,14 @@ export default function TabView() {
   ];
 
   const [curentTab, setCurentTab] = useState(0);
+
+  useEffect(() => {
+    if (location.hash) {
+      let found = tabs.filter((tab) => tab.header === location.hash.slice(1));
+      let foundIdx = tabs.indexOf(found[0]);
+      setCurentTab(foundIdx);
+    }
+  }, []);
 
   return (
     <div className="md:w-2/4 w-full px-3">
